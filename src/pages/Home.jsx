b0a47b2f0ca9e44a2c12a41493732d349c11d1ef
Nota0515 <businessbro78@gmail.com>
2025-04-05 17:57:43 +0530

    redeveloping home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div></div>
+    <div className='flex relative bg-red-200 justify-center items-center h-screen w-screen'>
+      <div className='absolute bottom-0 bg-red-700'>
+        <h1>Hi this is my main content</h1>
+      </div>
+    </div>
   )
 }
 
