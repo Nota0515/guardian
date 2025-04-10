803e83e32ff06c65df4883646adf14ff2e4f6bea
Nota0515 <businessbro78@gmail.com>
2025-04-10 16:01:03 +0530

    bug fixed

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const Button = ({children , className , href , onClick , type= "button" , disabled = false  }) => {
 
-    const classes = `${className || ''} ${disabled ? 'pointer-event-none' : ''} relative inline-flex items-center justify-center p-1`
+    const classes = `${className || ''} ${disabled ? 'cursor-not-allowed opacity-60' : ''} relative inline-flex items-center justify-center p-1`
   
     const renderButton = () =>(
-      <button className={classes} onClick={onClick} type={type}>
-          <span className="z-[2px] flex items-center gap-2 pointer-events-none">{children}</span>
+      <button className={classes} onClick={onClick} type={type} disabled={disabled}>
+          <span className="z-[2px] flex items-center gap-2 ">{children}</span>
       </button>
     );
   
