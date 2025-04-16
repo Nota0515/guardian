b1dc3fc538775b1eeca543b9b7a486702097b88c
Nota0515 <businessbro78@gmail.com>
2025-04-16 15:51:17 +0530

    removed overflow-hidden

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Pricing from "./pages/Pricing"
 const App = () => {
   return (
     <>
-      <div className="w-screen h-[100svh] bg-gradient-radial from-black via-black to-blue-600/10 overflow-hidden">
+      <div className="w-screen h-[100svh] bg-gradient-radial from-black via-black to-blue-600/10">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pricing" element={<Pricing />} />
