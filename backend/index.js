d25099304c68cbf86d483e1dc1a5f1f5810a3e3e
Nota0515 <businessbro78@gmail.com>
2025-05-06 17:58:41 +0530

    created signup auth routes on backend

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,15 @@
 const express = require('express')
+const cors = require("cors")
+const dotenv = require('dotenv')
+
+dotenv.config();
 const app = express();
+app.use(cors());
+app.use(express.json());
 
+app.use('/api/auth' , Authroutes);
+app.use('/api/protected' , protectedRoutes);
 
-app.get("/", (req , res)=>{
-    const value = res.sendDate;
-    res.send('hi this is server',value)
-})
+const Port = process.env.PORT || 5000 ;
 
-app.get('/signup' , (req , res)=>{
-    res.send("this is it")
-})
-
-app.get("/login" , (req,res)=>{
-    res.send("this is login page")
-})
-
-app.listen(3000)
+app.listen(Port , ()=> console.log(`server is started at ${Port}`))
