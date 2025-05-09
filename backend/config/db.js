8a4b3169d7b1b261b3557f9709422efe62e5cbf1
Nota0515 <businessbro78@gmail.com>
2025-05-09 17:04:11 +0530

    update into try catch based approach

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 
-function connectToDB (){
-    mongoose.connect(process.env.MONGO_URI)
-    .then(()=>console.log("mongoDB is connected"))
-    .catch(err => console.log(err))
-}
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("database is connect")
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+};
 
-module.exports = connectToDB ; 
+module.exports = connectToDB; 
