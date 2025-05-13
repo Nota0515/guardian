f95de315bc301ac8022f9e25334ac67a2dcc2513
Nota0515 <businessbro78@gmail.com>
2025-05-13 18:11:10 +0530

    created password hashing and compare logic

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,24 @@
 const mongoose = require('mongoose')
 
+//user ka boilerplate
+
 const UserSchema = new mongoose.Schema({
     email : { type: String , required : true , unique : true},
     password : {type: String , required : true }
-})
+}, {timestamps :true})
+
+//password ka protection bhi toh jaruri hai 
+UserSchema.pre('save',async function (next){
+    if(!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+
+//entered passward compare kero with the og passward 
+UserSchema.methods.matchPassword = function (entered){
+    return bcrypt.compare(entered, this.password )
+}
 
 module.exports = mongoose.model("User", UserSchema);
