df3ef5ec39df527c08336a02c330a825603870da
Nota0515 <businessbro78@gmail.com>
2025-05-17 23:12:43 +0530

    added authentication routes for signup and login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,63 +1,8 @@
-/*const router = require('express').Router;
-const JWT = require('jsonwebtoken');
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');*/
+const {signup , login } = require('../controller/authcontroller')
 const express = require('express');
 const router = express.Router();
 
-//middleware 
-
-const auth = function (req, res, next) {
-    console.log("auth wala middleware run hua");
-    req.user = { UserId: 390867356, role: "admin" };
-    if (req.user){
-        next();
-    }else{
-        console.log(":(")
-        res.json({
-            sucess:false,
-            message:"Not a valid user"
-        })
-    }
-}
-
-const isStudent = function (req , res , next ){
-    console.log("Abhi isstudent wla middleware chala");
-    if(req.user.role === "student"){
-        console.log("ha ye student hai")
-        next();
-    }else{
-        res.json({
-            sucess:false,
-            message:"not a student"
-        })
-    }
-}
-
-const isAdmin = function (req , res , next ){
-    console.log("Abhi isAdmin wla middleware chala");
-    if(req.user.role === "admin"){
-        console.log("ha ye admin hai")
-        next();
-    }else{
-        res.json({
-            sucess:false,
-            message:"not a admin"
-        })
-    }
-}
-
-//routes
-
-
-router.get('/student' , auth , isStudent , (req , res )=>{
-    console.log("I am inside student page")
-    res.send("student specific page")
-})
-
-router.get('/admin' , auth , isAdmin , (req ,res)=>{
-    console.log("I am in admin page");
-    res.send("Admin specific page");
-})
+router.post('/signup' , signup);
+router.post('/login' , login);
 
 module.exports = router;
