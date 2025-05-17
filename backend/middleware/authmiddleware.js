430effaca75842930059453c73f19939d47b0369
Nota0515 <businessbro78@gmail.com>
2025-05-17 23:05:53 +0530

    created an auth middleware

---
+++ backend/middleware/authmiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const user = require('../models/User');
+const User = require('../models/User');
+
+
+//this will run every time to Aujthenticate user req and project the routes from miselaneus 3rd party req or unauthurised req
+
+exports.protect = async (req , res , next ) => {
+    let token = req.headers.authorization?.split(' ')[1];
+    if (!token) return res.status(401).json({message : "invalid token, unauthorised"});
+    try{
+        const decoded = jwt.verify(token , process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select('-password');
+        next();
+    }catch(err){
+        res.status(401).json({message : "token Invalid or expires"});
+    }
+};
