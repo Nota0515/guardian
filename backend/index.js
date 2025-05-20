4e3e6b6f83881fb8126fd21740125e96dd9cae0b
Nota0515 <businessbro78@gmail.com>
2025-05-20 22:58:19 +0530

    add mondoDB call in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const dotenv = require('dotenv')
 const express = require('express')
 const cors = require("cors")
+const connectDB = require('./config/db')
 const route = require('./routes/authRoutes')
 
 dotenv.config();
 const app = express();
+connectDB();
 //first builtinmiddleware
 app.use(cors());
 //second builtinmiddleware
