dcd4225b9ab7fd3ec3707ffcd97f04efd8966ef9
Nota0515 <businessbro78@gmail.com>
2025-05-21 17:04:30 +0530

    protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,18 @@ import Signup from './pages/Signup'
 import Login from "./pages/Login"
 import Home from "./pages/Home"
 import Pricing from "./pages/Pricing"
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
     <>
       <div className="w-screen min-h-[100dvh] bg-gradient-radial from-black via-black to-blue-600/10">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          } />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
