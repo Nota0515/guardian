bf009bd7e27f5fbaed66561610b271e669ebf811
Nota0515 <businessbro78@gmail.com>
2025-05-22 16:08:07 +0530

    update to check the expry of the token

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,8 +1,17 @@
 import { Navigate } from "react-router-dom";
+import { jwtDecode } from 'jwt-decode';
 
 const PrivateRoute = ({children}) => {
     const token = localStorage.getItem('token');
-    return token ? children : <Navigate to='/login' />; 
+    if (!token) return <Navigate to='/login' />;
+    try {
+        const decode = jwtDecode(token);
+        const isExpired = decode.exp*1000 < Date.now();
+        return isExpired ? 
+            <Navigate to='/login' /> : children ;
+    } catch (error) {
+        return <Navigate to='/login' />;
+    }
 };
 
 export default PrivateRoute ;
