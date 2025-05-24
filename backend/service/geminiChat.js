5a056ef3e5d3a974086b718e304741ace51dc363
Nota0515 <businessbro78@gmail.com>
2025-05-24 17:50:48 +0530

    created error handling in api requests

--- backend/service/geminiChat.js
+++ backend/service/geminiChat.js
@@ -1,13 +1,22 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 require('dotenv').config();
 
-const genAi = new GoogleGenerativeAI(process.env.GEMINI_API);
+const apikeyG = process.env.GEMINI_API
+const genAi = new GoogleGenerativeAI(apikeyG);
 
 const generateChat = async (prompt) => {
-    const model = genAi.getGenerativeModel({model:'gemini-2.0-flash'});
-    const result = await model.generateContent(prompt);
-    const response = await result.response ; 
-    return response.text();
+    try {
+        console.log(prompt)
+        const model = genAi.getGenerativeModel({ model: 'gemini-2.5-pro-preview-05-06' });
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        return response.text();
+    } catch (error) {
+        if(error.status === 429){
+            throw new Error ("Quata exceeed . wait and try later.")
+        }
+        throw error ;   
+    }
 }
 
-module.exports = generateChat ; 
+module.exports = generateChat;
