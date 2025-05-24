498e3b5a006927259e82ceeb176db464c5ec5b57
Nota0515 <businessbro78@gmail.com>
2025-05-24 17:49:39 +0530

    import bug fixed

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,4 +1,4 @@
-const { generateChat } = require('../service/geminiChat');
+const  generateChat  = require('../service/geminiChat');
 const express = require('express');
 const router = express.Router();
 
