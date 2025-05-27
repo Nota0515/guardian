0c34d7f78338516be7676718b679b751b1421c93
Nota0515 <businessbro78@gmail.com>
2025-05-27 17:41:26 +0530

    updated comment

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const handleChat = require('../controller/chatController')
 
-//post /api/chat
+// /api/chat
 
 router.post('/chat' , handleChat );
 
