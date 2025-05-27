2444a71b524ab3da822a776e28b2e44f801080ab
Nota0515 <businessbro78@gmail.com>
2025-05-27 17:40:16 +0530

    develop routes for chat req

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,18 +1,9 @@
-const  generateChat  = require('../service/geminiChat');
 const express = require('express');
 const router = express.Router();
+const handleChat = require('../controller/chatController')
 
 //post /api/chat
 
-router.post('/chat' , async(req , res)=>{
-    const { prompt } = req.body;
-    try {
-        const reply = await generateChat(prompt);
-        res.json({response : reply});
-    } catch (error) {
-        res.status(500).json({message : "genration error or " , error})
-        console.log(error);
-    }
-});
+router.post('/chat' , handleChat );
 
 module.exports = router ; 
