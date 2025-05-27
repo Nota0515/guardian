df7d905f3a21696400c2af56a8f54b689875fe92
Nota0515 <businessbro78@gmail.com>
2025-05-27 17:50:40 +0530

    bug fixed imports error

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const handleChat = require('../controller/chatController')
+const { handleChat } = require('../controller/chatController')
 
 // /api/chat
 
