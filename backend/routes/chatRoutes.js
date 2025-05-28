462578ee41278ea7cdb3b73a42fb4c013e0f7734
Nota0515 <businessbro78@gmail.com>
2025-05-28 15:22:24 +0530

    protected the chat api route

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { handleChat } = require('../controller/chatController')
+const { protect } = require('../middleware/authmiddleware')
+
+
 
 // /api/chat
 
-router.post('/chat' , handleChat );
+router.post('/chat' ,  protect , handleChat );
 
 module.exports = router ; 
