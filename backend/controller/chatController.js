cf2b4e86227ab055150ac406f1f1e1a32f24ea15
Nota0515 <businessbro78@gmail.com>
2025-05-31 19:15:44 +0530

    updated file according to the new client req

--- backend/controller/chatController.js
+++ backend/controller/chatController.js
@@ -1,9 +1,9 @@
 const generateChat = require('../service/geminiChat')
 
 const handleChat = async(req , res) => {
-    const {prompt} = req.body ;
+    const {messages} = req.body ;
     try {
-        const response = await generateChat(prompt) ; 
+        const response = await generateChat(messages) ; 
         res.status(200).json({ response })
     } catch (error) {
         res.status(500).json({ error : "failed in the controller ig"})
