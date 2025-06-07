88c8fb496680609c9edd32921b321fa77a1b25c7
Nota0515 <businessbro78@gmail.com>
2025-06-07 22:46:31 +0530

    bug fixed

--- frontend/src/components/SidebarCon.jsx
+++ frontend/src/components/SidebarCon.jsx
@@ -6,14 +6,14 @@ const SidebarCon = ({ chats, activeChatid, onSelect }) => {
         <div className='sidebar-content w-64 pt-16 px-2'>
             <h2 className="text-xl font-medium mb-4">Recent Chats</h2>
             <div className="chat-list space-y-2">
-                {ChartArea.length === 0 ? (
+                {chats.length === 0 ? (
                     <div className='nopastchat text-gray-400 italic'>No History</div>
                 ) : (
                     chats.map(chat => (
                         <div
                             key={chat._id}
                             onClick={() => onSelect(chat._id)}
-                            className={`chat-item p-2 rounded 
+                            className={`chat-item p-2 mr-4 rounded 
                                 ${chat._id === activeChatid ? "bg-white/20" : "hover:bg-white/10" }
                                  cursor-pointer`}
                         >
