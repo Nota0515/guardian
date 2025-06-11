f447e0f6d712f177476008a4f3fddb189b87b9f5
Nota0515 <businessbro78@gmail.com>
2025-06-11 18:54:08 +0530

    replace avtice chat state with the new chatId param

--- frontend/src/components/SidebarCon.jsx
+++ frontend/src/components/SidebarCon.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const SidebarCon = ({ chats, activeChatid, onSelect }) => {
+const SidebarCon = ({ chats, chatId, onSelect }) => {
     return (
         <div className='sidebar-content w-64 pt-16 px-2'>
             <h2 className="text-xl font-medium mb-4">Recent Chats</h2>
@@ -14,7 +14,7 @@ const SidebarCon = ({ chats, activeChatid, onSelect }) => {
                             key={chat._id}
                             onClick={() => onSelect(chat._id)}
                             className={`chat-item p-2 mr-4 rounded 
-                                ${chat._id === activeChatid ? "bg-white/20" : "hover:bg-white/10" }
+                                ${chat._id === chatId ? "bg-white/20" : "hover:bg-white/10" }
                                  cursor-pointer`}
                         >
                             {chat.title}
