602beb524eed9e546f56a91bb3ae7570850735b8
Nota0515 <businessbro78@gmail.com>
2025-06-22 22:28:50 +0530

    updated SidebarCon.jsx

--- frontend/src/components/SidebarCon.jsx
+++ frontend/src/components/SidebarCon.jsx
@@ -1,13 +1,27 @@
-import {BsThreeDots} from 'react-icons/bs';
+import { BsThreeDots } from 'react-icons/bs';
 import Button from './Buttons';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 
-const SidebarCon = ({ chats, chatId, onSelect , onRename , onDelete }) => {
-    const [openMenuId , setOpenMenuId ] = useState(null);
+const SidebarCon = ({ chats, chatId, onSelect, onRename, onDelete }) => {
+    const menuref = useRef(null);
+
+    const [openMenuId, setOpenMenuId] = useState(null);
     const toggleMenu = (id) => {
         setOpenMenuId(prev => (prev === id ? null : id));
     }
+
+    useEffect((e) => {
+        const handleOutClick = (e) => {
+            if (menuref.current && !menuref.current.contains(e.target)) {
+                setOpenMenuId(null);
+            }
+        };
+        document.addEventListener('click', handleOutClick);
+        return () => document.removeEventListener('click', handleOutClick);
+    }, []);
+
+
     return (
         <div className='sidebar-content2 pt-5 px-2 min-h-0 flex flex-col'>
             <h2 className="text-xs font-semibold font-mainFont text-gray-500 mb-4 ml-2 shrink-0">Recent</h2>
@@ -19,40 +33,44 @@ const SidebarCon = ({ chats, chatId, onSelect , onRename , onDelete }) => {
                         <div
                             key={chat._id}
                             onClick={() => onSelect(chat._id)}
-                            className={`chat-item p-2 mr-2 flex relative text-gray-200 text-sm justify-between items-center group font-mainFont rounded-xl
-                                ${chat._id === chatId ? "bg-blue-300/20" : "md:hover:bg-blue-300/10" }
+                            className={`chat-item p-2 mr-2 flex relative ${openMenuId === chat._id ? "bg-blue-300/20" : ''} text-gray-200 text-sm justify-between items-center group font-mainFont rounded-xl
+                                ${chat._id === chatId ? "bg-blue-300/10 md:hover:bg-blue-300/20" : "md:hover:bg-blue-300/20"}
                                  cursor-pointer`}
                         >
                             <span onClick={() => onSelect(chat._id)} className="truncate pl-1">{chat.title}</span>
 
                             {/*here we will create the 3dot hover effect */}
-                            <div className='flex opacity-100 md:opacity-0 md:group-hover:opacity-100 pr-2 items-center transition-opacity duration-75'>
-                                <Button onClick={(e)=>{
+                            <div className={`flex opacity-100  ${openMenuId === chat._id ? 'opacity-100' : 'md:opacity-0 md:group-hover:opacity-100'} pr-2 items-center transition-opacity duration-75`}>
+                                <Button onClick={(e) => {
                                     e.stopPropagation();
-                                    toggleMenu(chat._id)}}>
+                                    toggleMenu(chat._id)
+                                }}>
                                     <BsThreeDots />
                                 </Button>
                             </div>
                             {/*the menu toggle will open on the basis of the openmenuID mechanism */}
                             {openMenuId === chat._id &&
-                            <div className='absolute flex flex-col items-center justify-center gap-y-2  right-2 top-10 w-20 h-20 bg-black border border-white/20 text-sm rounded-md z-10 py-1 px-2 space-y-1'>
-                                <button className='block hover:bg-gray-700 text-gray-500'
-                                onClick={()=>{
-                                    onRename(chat._id)
-                                    setOpenMenuId(null);
-                                }}
-                                >
-                                    rename
-                                </button>
-                                <button className='block text-red-600 hover:bg-red-600/10 '
-                                onClick={()=>{
-                                    onDelete(chat._id)
-                                    setOpenMenuId(null);
-                                }}
-                                >
-                                    delete
-                                </button>
-                            </div>
+                                <div ref={menuref} className='absolute flex flex-col justify-center right-1 top-10 bg-gray-950 border border-white/20 text-base font-mainFont rounded-lg p-1 z-10'>
+                                    <button className='block hover:bg-gray-800 p-1 rounded-md px-3 text-gray-300'
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            onRename(chat._id)
+                                            setOpenMenuId(null);
+                                        }}
+                                    >
+                                        rename
+                                    </button>
+                                    <div className='w-full border border-white/10 my-1'></div>
+                                    <button className='block text-red-400 p-1 rounded-md hover:bg-orange-950 '
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            onDelete(chat._id)
+                                            setOpenMenuId(null);
+                                        }}
+                                    >
+                                        delete
+                                    </button>
+                                </div>
                             }
                         </div>
                     ))
@@ -62,5 +80,5 @@ const SidebarCon = ({ chats, chatId, onSelect , onRename , onDelete }) => {
     );
 };
 
-export default SidebarCon ;
+export default SidebarCon;
 
