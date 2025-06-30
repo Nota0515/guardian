7b7b4cb48b19afb60ae5ec46a4e4ef51d29c5b74
Nota0515 <businessbro78@gmail.com>
2025-06-30 20:22:59 +0530

    created sidebar loading skeleton for display while fetching

---
+++ frontend/src/components/Sidebarskeleton.jsx
@@ -0,0 +1,18 @@
+
+const Sidebarskeleton = () => {
+    return (
+        <div className="pt-5 px-2 min-h-0 pb-10 flex flex-col ">
+            <div class="overflow-hidden rounded-lg flex flex-col space-y-6 pt-5 shadow-3">
+                <span class="mx-2 mt-3 inline-block h-[30px] w-[40%] animate-[pulse_1s_ease-in-out_infinite] bg-gray-800 rounded-lg "></span>
+                <span class="mx-2  inline-block h-[30px] w-[70%] animate-[pulse_1s_ease-in-out_infinite] bg-gray-800 rounded-lg "></span>
+                <span class="mx-2  inline-block h-[30px] w-[50%] animate-[pulse_1s_ease-in-out_infinite] bg-gray-800 rounded-lg "></span>
+                <span class="mx-2  inline-block h-[30px] w-[80%] animate-[pulse_1s_ease-in-out_infinite] bg-gray-800 rounded-lg "></span>
+                <span class="mx-2  inline-block h-[30px] w-[70%] animate-[pulse_1s_ease-in-out_infinite] bg-gray-800 rounded-lg "></span>
+                <span class="mx-2  inline-block h-[30px] w-[50%] animate-[pulse_1s_ease-in-out_infinite] bg-gray-800 rounded-lg "></span>
+                <span class="mx-2  inline-block h-[30px] w-[80%] animate-[pulse_1s_ease-in-out_infinite] bg-gray-800 rounded-lg "></span>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebarskeleton
